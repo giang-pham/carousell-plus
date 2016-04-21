79cfe0b2cad42081219599b4ce67fd71ee4b2fde
Hoang Pham <hoang_pham@aviva-asia.com>
2016-04-21 16:25:18 +0800

    change component name

--- app/product-card.component.ts
+++ app/product-card.component.ts
@@ -4,25 +4,27 @@ import 'rxjs/add/operator/map';
 import {TruncatePipe} from './truncate';
 
 @Component({
-  selector: 'hello-component',
+  selector: 'product-card',
   viewProviders: [HTTP_PROVIDERS],
   pipes: [TruncatePipe],
   template: `
   <input #searchStr placeholder="books, camera, games, ..." />
   <button (click)="getProductList(searchStr.value)">Search</button>
-  <div class="card-columns">
-    <div *ngFor="#item of items; #i = index" class="card">
-      <img class="card-img-top" data-src="{{item.primary_photo_url}}" alt="Card image cap">
-      <div class="card-block">
-        <h4 class="card-title">{{item.title}}</h4>
-        <p class="card-text">{{item.description | truncate : letterLimit}}</p>
-        <a href="#" class="btn btn-primary">PM</a>
+  <div class="row">
+    <div *ngFor="#item of items; #i = index" class="col-sm-3">
+      <div class="card">
+        <img class="card-img-top" data-src="{{item.primary_photo_url}}" alt="{{item.description | truncate : letterLimit}}" style="height: 180px; width: 100%; display: block;">
+        <div class="card-block">
+          <p class="card-text">{{item.title | truncate : 20 }}</p>
+          <!-- <p class="card-text">{{item.description | truncate : letterLimit}}</p> -->
+          <a href="https://carousell.com/p/{{item.id}}" class="btn btn-primary">PM</a>
+        </div>
       </div>
     </div>
   </div>
   `
 })
-export class HelloComponent {
+export class ProductCardComponent {
   items:Object[] = [];
   letterLimit:number = 100;
   searchStr:string = '';
